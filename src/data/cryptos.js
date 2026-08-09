211e2a21ce7611ebbbc296d8394098223b9fcfe8
facundolgomez <facundolgomez87@gmail.com>
2026-08-09 19:47:29 -0300

    paso el arreglo a un nuevo archivo

---
+++ src/data/cryptos.js
@@ -0,0 +1,42 @@
+export const cryptos = [
+  {
+    id: 1,
+    name: "Bitcoin",
+    symbol: "BTC",
+    amount: 0.15,
+    value: 9750,
+    image:
+      "https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400",
+    change: 2.4,
+  },
+  {
+    id: 2,
+    name: "Ethereum",
+    symbol: "ETH",
+    amount: 1.8,
+    value: 4500,
+    image:
+      "https://assets.coingecko.com/coins/images/279/standard/ethereum.png?1696501628",
+    change: -1.1,
+  },
+  {
+    id: 3,
+    name: "Solana",
+    symbol: "SOL",
+    amount: 20,
+    value: 1600,
+    image:
+      "https://assets.coingecko.com/coins/images/4128/standard/solana.png?1718769756",
+    change: 5.7,
+  },
+  {
+    id: 4,
+    name: "Cardano",
+    symbol: "ADA",
+    amount: 1500,
+    value: 900,
+    image:
+      "https://assets.coingecko.com/coins/images/975/standard/cardano.png?1696502090",
+    change: 0.8,
+  },
+];
