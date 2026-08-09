844964f3472fc6753c34c7f7699d128d7248ed5a
facundolgomez <facundolgomez87@gmail.com>
2026-08-09 19:50:00 -0300

    modifico el componente CryptoItem

--- src/components/CryptoItem.jsx
+++ src/components/CryptoItem.jsx
@@ -1,16 +1,21 @@
-const CryptoItem = ({ cryptoName, amount, value }) => {
+const CryptoItem = ({ cryptoName, symbol, amount, value, image }) => {
   //   const cryptoName = "Bitcoin";
   //   const amount = "0.15 BTC";
   //   const value = "USD 9.750";
 
   return (
-    <div>
-      <h3>{cryptoName} BTC</h3>
-      <p>{amount} BTC</p>
-      <span>
-        <b>USD {value}</b>
-      </span>
-    </div>
+    <tr>
+      <td>
+        <div className="flex items-center gap-2">
+          <img className="w-8 h-8" src={image} alt={cryptoName} />
+          <span>{cryptoName}</span>
+        </div>
+      </td>
+      <td>
+        {amount} {symbol}
+      </td>
+      <td>{value} USD</td>
+    </tr>
   );
 };
 
